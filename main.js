0bb72790483f049dc565eac398de1ab8e7edd44c
Tyler Leonhardt <tylerl0706@gmail.com>
2016-02-02 02:10:49 -0500

    electron with material-angular

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/src/app/index.html');
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
